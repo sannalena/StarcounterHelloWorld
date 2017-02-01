e0cd51d3e82a8654539687fca9d8f05abb7fc657
ubbeK <urban.nyden@gmail.com>
2017-02-01 16:40:12 +0100

    My first commit

---
+++ StarcounterHelloWorld/Helloworld/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Starcounter;
+
+namespace HelloWorld
+{
+    [Database]
+    public class Person
+    {
+        public string FirstName;
+        public string LastName;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Db.Transact(() =>
+            {
+                var anyone = Db.SQL<Person>("SELECT p FROM Person p").First;
+                if (anyone == null)
+                {
+                    new Person
+                    {
+                        FirstName = "John",
+                        LastName = "Doe"
+                    };
+                }
+            });
+
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
+
+            Handle.GET("/HelloWorld", () =>
+            {
+                var person = Db.SQL<Person>("SELECT p FROM Person p").First;
+                var json = new PersonJson
+                {
+                    Data = person
+                };
+
+                if (Session.Current == null)
+                {
+                    Session.Current = new Session(SessionOptions.PatchVersioning);
+                }
+                json.Session = Session.Current;
+                return json;
+            });
+
+
+        }
+    }
+}
